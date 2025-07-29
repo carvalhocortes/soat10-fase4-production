4de79c7d4a3e4e45464a1e5e5a8ee75cda56f622
CarvalhoCortes <carvalhocortes@gmail.com>
2025-07-29 20:54:19 -0300

    fix: adicionar logs de conexão e erro no arquivo index.ts e remover logs de variáveis de ambiente do ormconfig.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { AppDataSource } from '@infrastructure/db/config/database.config';
 import Server from '@infrastructure/web/server';
 
+let isConnected = false;
+
 const start = async () => {
   try {
     await AppDataSource.initialize();
-    console.log('Conectado ao MySQL com TypeORM');
+    isConnected = true;
+    console.log('✅ Conectado ao MySQL com TypeORM');
     const server = new Server();
     server.start();
   } catch (error) {
-    console.log('Falha ao iniciar aplicação:', error);
+    console.log('❌ Falha ao iniciar aplicação:', error);
     process.exit(1);
   }
 };
@@ -16,6 +19,10 @@ const start = async () => {
 start();
 
 const shutdown = async () => {
+  if (!isConnected) {
+    console.log('❌ Aplicação não está conectada ao banco de dados.');
+    return;
+  }
   await AppDataSource.destroy();
   process.exit(0);
 };
