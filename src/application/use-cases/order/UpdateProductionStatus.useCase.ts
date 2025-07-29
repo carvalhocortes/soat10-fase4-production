70fae52a521a1a4e0936e7eaf37fad86f7a1f695
Samuel Victor <samuel@samuelvictor.dev>
2025-07-29 02:11:13 -0300

    adiciona emissão de evento

--- src/application/use-cases/order/UpdateProductionStatus.useCase.ts
+++ src/application/use-cases/order/UpdateProductionStatus.useCase.ts
@@ -1,10 +1,14 @@
 import { UpdateProductionStatusDTO } from '@application/dto/order/UpdateProductionStatus.dto';
 import { Order } from '@core/entities/order.entity';
+import { SnsPublisher } from '@infrastructure/external/snsPublisher';
 import { OrderRepository } from '@interfaces/gateways/OrderRepository.gateway';
 import { NotFoundError } from '@shared/errors/NotFoundError';
 
 export class UpdateProductionStatusUseCase {
-  constructor(private readonly orderRepository: OrderRepository) {}
+  constructor(
+    private readonly orderRepository: OrderRepository,
+    private readonly snsPublisher: SnsPublisher,
+  ) {}
 
   async execute({ id, status }: UpdateProductionStatusDTO): Promise<Order | null> {
     const order = await this.orderRepository.findById(id);
@@ -14,6 +18,11 @@ export class UpdateProductionStatusUseCase {
     }
 
     order.updateStatus(status);
+
+    await this.snsPublisher.publish({
+      eventType: 'PRODUCTION_STATUS_UPDATED',
+      payload: order,
+    });
 
     return this.orderRepository.update(id, order);
   }
