d2c8e6408959e3a4fb399719dab162a9f540d096
CarvalhoCortes <carvalhocortes@gmail.com>
2025-07-29 22:11:35 -0300

    fix: adicionar parâmetro _next ao manipulador de erros para compatibilidade com middleware do Express

--- src/infrastructure/web/middlewares/errorHandler.middleware.ts
+++ src/infrastructure/web/middlewares/errorHandler.middleware.ts
@@ -1,14 +1,15 @@
 import { BaseError } from '@shared/errors/BaseError';
 import { NotFoundError } from '@shared/errors/NotFoundError';
 import { ValidationError } from '@shared/errors/ValidationError';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 const assembleError = <T extends BaseError>(err: T) => ({
   message: err.message,
   code: err.code,
 });
 
-const errorHandler = (err: Error, _req: Request, res: Response) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const errorHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
   if (err instanceof NotFoundError) {
     res.status(404).json(assembleError(err));
     return;
